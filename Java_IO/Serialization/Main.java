76b505c233437fe95cf6f592af35986f3acf0878
hosun1997 <hwanghsun@suwon.ac.kr>
2022-10-12 11:39:28 +0900

    serializaion -ing

--- Java_IO/Serialization/Main.java
+++ Java_IO/Serialization/Main.java
@@ -1,32 +1,101 @@
 package Java_IO.Serialization;
 
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.IOException;
-import java.io.ObjectOutputStream;
+import java.io.*;
+import java.nio.ByteBuffer;
+import java.util.Arrays;
 import java.util.Base64;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+//        saveObject();
+//        loadObject();
+        serialization();
+    }
 
-        File file = new File("Serialize.txt");
+    public static void saveObject() {
+        Member steve = new Member("Steve", 25);
+        Member james = new Member("James", 30);
+//        System.out.println(steve);
+//        System.out.println(james);
 
-        Member member = new Member("Steve", "Steve@naver.com", 25);
-        byte[] serializedMember = new byte[0];
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
         try {
-            ObjectOutputStream oos =  new ObjectOutputStream(baos);
-            oos.writeObject(member); // serializeMember = 직렬화된 맴버 객체
-            serializedMember = baos.toByteArray();
+            FileOutputStream fileOut = new FileOutputStream("./saveObject.txt");
+            DataOutputStream dataOut = new DataOutputStream(fileOut);
+            dataOut.writeUTF(steve.getName());
+            dataOut.writeInt(steve.getAge());
+            dataOut.writeUTF(james.getName());
+            dataOut.writeInt(james.getAge());
+            fileOut.close();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // txt 파일을 읽었는데 이상한 값들이 있다. 왜 그럴까?
+
+    }
+
+    public static void loadObject() throws IOException {
+        FileInputStream fileIn = new FileInputStream("./saveObject.txt");
+        DataInputStream dataIn = new DataInputStream(fileIn);
+
+        byte[] data = fileIn.readNBytes(4);
+        System.out.println(Arrays.toString(data));
+        System.out.println(ByteBuffer.wrap(data).getInt());
+//        byte[] name = new byte[10];
+//        fileIn.read(name, 3, 7);
+        byte[] name = fileIn.readNBytes(4);
+        fileIn.read(name, 0, 3);
+        System.out.println(Arrays.toString(name));
+        System.out.println(new String(name));
+        System.out.println(Arrays.toString(fileIn.readNBytes(4)));
+        System.out.println(fileIn.readAllBytes().length);
+
+
+        /* 2번
+        String name = dataIn.readUTF();
+        int age = dataIn.readInt();
+        Member steve = new Member(name, age);
+
+        name = dataIn.readUTF();
+        age = dataIn.readInt();
+        Member james = new Member(name, age);
+
+        System.out.println("Load Complete!!!");
+        System.out.println(steve);
+        System.out.println(james);
+         */
+
+        /* 1번
+        System.out.println(dataIn.readUTF());
+        System.out.println(dataIn.readInt());
+        System.out.println(dataIn.readUTF());
+        System.out.println(dataIn.readInt());
+        */
+
+
+    }
+
+    public static void serialization() {
+        Member steve = new Member("Steve", 25);
+        Member james = new Member("James", 30);
+
+        try {
+            FileOutputStream fileOut = new FileOutputStream("./SerialObject.txt");
+            ObjectOutputStream objOut = new ObjectOutputStream(fileOut);
+            objOut.writeObject(steve);
+            objOut.writeObject(james);
 
         } catch (IOException e) {
             e.printStackTrace();
         }
-        System.out.println(Base64.getEncoder().encodeToString(serializedMember)); // 바이트 배열로 생성된 직렬화데이터를 base64로 변환
-
     }
 
+
 }
+
 
 //  <직렬화 사용 이유>
 //  생성한 객체를 파일로 저장할 일이 있을 수도 있습니다.
--- Java_IO/Serialization/Member.java
+++ Java_IO/Serialization/Member.java
@@ -8,14 +8,40 @@ public class Member implements Serializable {
     private String email;
     private int age;
 
-    public Member(String name, String email, int age) {
+    public Member(String name, int age) {
         this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
         this.email = email;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
         this.age = age;
     }
 
     @Override
     public String toString() {
-        return String.format("Member{name='%s', email='%s', age='%s'}", name, email);
+        return "Member{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
     }
 }
